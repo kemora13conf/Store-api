131aa43b3b394c2fc840daccc5e293e29ec280a4
kemora_13conf <Abdomouak48@gmail.com>
2023-11-14 18:18:15 +0100

    Done With orders, pdf template for order invoice needed. x_0!

--- Runner.js
+++ Runner.js
@@ -5,10 +5,10 @@ const port = 8080
 
 // make the server run on the local network as well
 
-// app.listen(port, '0.0.0.0', () => {
-//     //   console.log(os.networkInterfaces())
-//     const localIp = os.networkInterfaces().wlo1[0].address;
-//     console.log(`Server is running at http://${localIp}:${port}`);
-// });
+app.listen(port, '0.0.0.0', () => {
+//       console.log(os.networkInterfaces())
+    const localIp = os.networkInterfaces()['Wi-Fi'][1].address;
+    console.log(`Server is running at http://${localIp}:${port}`);
+});
 
-app.listen(port, console.log(`Server running on http://localhost:${port}`))
+// app.listen(port, console.log(`Server running on http://localhost:${port}`))
--- Server/Routes/Orders/Controller.js
+++ Server/Routes/Orders/Controller.js
@@ -4,6 +4,8 @@ import { faker } from "@faker-js/faker";
 import Status from "../../Models/Status.js";
 import Product from "../../Models/Product.js";
 import Item from "../../Models/Item.js";
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+import fs from "fs";
 
 const orderById = async (req, res, next, id) => {
   try {
@@ -32,6 +34,91 @@ const orderById = async (req, res, next, id) => {
   }
 };
 
+async function generateInvoicePDF({ _id, amount, client, items }) {
+  const pdfDoc = await PDFDocument.create();
+  const page = pdfDoc.addPage([600, 400]);
+  const { height } = page.getSize();
+
+  const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+
+  // Set font and text
+  page.drawText("Invoice", {
+    x: 50,
+    y: height - 50,
+    font,
+    fontSize: 30,
+    color: rgb(0, 0, 0),
+  });
+
+  let yPosition = height - 100;
+
+  // Iterate through items and add them to the PDF
+  items.forEach((item) => {
+    yPosition -= 20;
+    page.drawText(
+      `${item.product.name} - ${item.quantity} x $${item.product.price}`,
+      { x: 50, y: yPosition, font, fontSize: 12, color: rgb(0, 0, 0) }
+    );
+  });
+
+  yPosition -= 20;
+  page.drawText(`Total Amount: $${amount}`, {
+    x: 50,
+    y: yPosition,
+    font,
+    fontSize: 12,
+    color: rgb(0, 0, 0),
+  });
+
+  const pdfBytes = await pdfDoc.save();
+  const pdfNmae = `${_id}-${new Date().getTime()}-invoice.pdf`;
+  const PdfPath = `Public/Invoices/${pdfNmae}`;
+  fs.writeFileSync(PdfPath, pdfBytes);
+  return pdfNmae;
+}
+
+const invoice = async (req, res) => {
+  const { lang } = req;
+  const { order } = req;
+  try {
+    const invoice = {
+      _id: order._id,
+      transaction_id: order.transaction_id,
+      amount: order.amount,
+      status: order.paid ? lang.paid : lang.unpaid,
+      client: {
+        fullname: order.client.fullname,
+        email: order.client.email,
+        image: order.client.image,
+      },
+      items: order.items.map((item) => {
+        return {
+          product: {
+            name: item.product.name,
+            price: item.product.price,
+          },
+          quantity: item.quantity,
+          amount: item.amount,
+        };
+      }),
+    };
+    const pdf = await generateInvoicePDF(invoice);
+    return res
+      .status(200)
+      .json(response("success", lang.data_fetched_successfully, pdf));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json(
+        response(
+          "error",
+          lang.something_wrong + " " + lang.fetching_data + ". " + error.message
+        )
+      );
+  }
+};
+
 const list = async (req, res) => {
   const { lang } = req;
   let { orderby, page, limit } = req.query;
@@ -48,6 +135,10 @@ const list = async (req, res) => {
           populate: {
             path: "product",
             model: "Product",
+            populate: {
+              path: "gallery",
+              model: "Image",
+            },
           },
         })
         .populate("client", "fullname image email")
@@ -60,34 +151,41 @@ const list = async (req, res) => {
           populate: {
             path: "product",
             model: "Product",
+            populate: {
+              path: "gallery",
+              model: "Image",
+            },
           },
         })
         .populate("client", "fullname image email")
         .sort({ createdAt: -1 });
     } else if (orderby === lang.status) {
       orders = await Order.find()
-      .populate("status")
-      .populate({
-        path: "items",
-        populate: {
-          path: "product",
-          model: "Product",
-        },
-      })
-      .populate("client", "fullname image email")
-      
+        .populate("status")
+        .populate({
+          path: "items",
+          populate: {
+            path: "product",
+            model: "Product",
+            populate: {
+              path: "gallery",
+              model: "Image",
+            },
+          },
+        })
+        .populate("client", "fullname image email");
+
       let statusOrder = await Status.find({}); // You should replace this with a valid ObjectId from your database
       statusOrder = statusOrder
-                      .map((status) => status.name)
-                      .sort((a, b) => a.localeCompare(b));
-  
+        .map((status) => status.name)
+        .sort((a, b) => a.localeCompare(b));
+
       const ordersArr = [];
       statusOrder.forEach((status) => {
         const order = orders.filter((order) => order.status.name === status);
         ordersArr.push(...order);
       });
       orders = ordersArr;
-
     } else {
       orders = await Order.find()
         .populate("status")
@@ -96,6 +194,10 @@ const list = async (req, res) => {
           populate: {
             path: "product",
             model: "Product",
+            populate: {
+              path: "gallery",
+              model: "Image",
+            },
           },
         })
         .populate("client", "fullname image email");
@@ -178,19 +280,19 @@ const create = async (req, res) => {
 
 const update_status = async (req, res) => {
   const { order, lang, currentUser } = req;
-  if (!currentUser.can_edit_order()){
+  if (!currentUser.can_edit_order()) {
     return res.status(401).json(response("error", lang.no_permission));
   }
   const { status } = req.body;
   try {
-    const statusId = await Status.findOne({name: status})
-    if(!statusId) return res.status(400).json(response('error', lang.status_not_found))
+    const statusId = await Status.findOne({ name: status });
+    if (!statusId)
+      return res.status(400).json(response("error", lang.status_not_found));
     const newOrder = await Order.findOneAndUpdate(
       { _id: order._id },
       { status: statusId },
       { new: true }
-    )
-    .populate("status");
+    ).populate("status");
     return res
       .status(200)
       .json(
@@ -218,7 +320,8 @@ const update_status = async (req, res) => {
   }
 };
 
-const deleteMultiple = async (req, res) => {const { lang, currentUser } = req;
+const deleteMultiple = async (req, res) => {
+  const { lang, currentUser } = req;
   if (!currentUser.can_delete_order())
     return res.status(401).json(response("error", lang.no_permission));
 
@@ -248,6 +351,7 @@ const deleteMultiple = async (req, res) => {const { lang, currentUser } = req;
 
 export {
   orderById,
+  invoice,
   list,
   create,
   stateList,
