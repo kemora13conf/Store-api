56c7d226c62d94e2d9b8d20f49d7b8d0818dedb4
kemora_13conf <Abdomouak48@gmail.com>
2023-12-07 16:27:08 +0100

    Done with the dashboard. x_0!

--- Server/Routes/Analytics/Controller.js
+++ Server/Routes/Analytics/Controller.js
@@ -3,7 +3,12 @@ import Category from "../../Models/Category.js";
 import Order from "../../Models/Order.js";
 import Product from "../../Models/Product.js";
 import { response } from "../../utils.js";
+import Status from "../../Models/Status.js";
 
+const parseYear = (req, res, next, year) => {
+  req.year = year ? parseInt(year) : new Date().getFullYear();
+  next();
+};
 const total = async (req, res) => {
   const { lang } = req;
   try {
@@ -14,10 +19,10 @@ const total = async (req, res) => {
 
     res.status(200).json(
       response("success", lang.data_fetched_successfully, {
-        "categories": categories,
-        "products": products,
-        "orders": orders,
-        "clients": clients,
+        categories: categories,
+        products: products,
+        orders: orders,
+        clients: clients,
       })
     );
   } catch (error) {
@@ -34,7 +39,7 @@ const total = async (req, res) => {
 };
 const order_per_month = async (req, res) => {
   let { lang, year } = req;
-  
+
   try {
     let orders = await Order.aggregate([
       {
@@ -79,5 +84,118 @@ const order_per_month = async (req, res) => {
       );
   }
 };
+const amount_per_month = async (req, res) => {
+  let { lang, year } = req;
 
-export { total, order_per_month };
+  try {
+    let orders = await Order.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: new Date(`${year}-01-01T00:00:00.000Z`),
+            $lt: new Date(`${year}-12-31T23:59:59.999Z`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$createdAt" },
+          total: { $sum: "$amount" },
+        },
+      },
+      {
+        $project: {
+          month: "$_id",
+          total: 1,
+          _id: 0,
+        },
+      },
+      {
+        $sort: { month: 1 },
+      },
+    ]);
+    res.status(200).json(
+      response("success", lang.data_fetched_successfully, {
+        orders,
+      })
+    );
+  } catch (error) {
+    res
+      .status(400)
+      .json(
+        response(
+          "error",
+          `${lang.something_wrong} ${lang.fetching} ${lang.data}!`,
+          error
+        )
+      );
+  }
+}
+const recent_orders = async (req, res) => {
+  let { lang } = req;
+  try {
+    let orders = await Order.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .populate("status")
+      .populate({
+        path: "items",
+        populate: {
+          path: "product",
+          model: "Product",
+        }
+      })
+      .populate("client", "fullname image email")
+    res.status(200).json(
+      response("success", lang.data_fetched_successfully, {
+        orders,
+      })
+    );
+  } catch (error) {
+    res
+      .status(400)
+      .json(
+        response(
+          "error",
+          `${lang.something_wrong} ${lang.fetching} ${lang.data}!`,
+          error
+        )
+      );
+  }
+};
+const invoice = async (req, res) => {
+  let { lang } = req;
+  try {
+    let orders = await Order.find()
+      .populate("status")
+    let status = await Status.find();
+
+    let count = [];
+    status.map((item) => {
+      let i = 0;
+      orders.map((order) => {
+        if (order.status.name === item.name) {
+          i++;
+        }
+      });
+      count.push({status: item.name, total: i});
+    });
+    res.status(200).json(
+      response("success", lang.data_fetched_successfully, {
+        orders: count,
+      })
+    );
+  } catch (error) {
+    res
+      .status(400)
+      .json(
+        response(
+          "error",
+          `${lang.something_wrong} ${lang.fetching} ${lang.data}!`,
+          error
+        )
+      );
+  }
+}
+
+export { total, order_per_month, amount_per_month, parseYear, recent_orders, invoice };
--- Server/Routes/Analytics/index.js
+++ Server/Routes/Analytics/index.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { signinRequired } from '../Auth/Controller.js'
-import { total, order_per_month } from './Controller.js';
+import { total, order_per_month, amount_per_month, parseYear, recent_orders, invoice } from './Controller.js';
 
 const router = new Router();
 
-router.param('year', (req, res, next, year)=>{
-  req.year = year ? parseInt(year) : new Date().getFullYear();
-  next();
-});
+router.param('year', parseYear);
 
 router.get('/total', signinRequired, total);
+router.get('/invoice', signinRequired, invoice);
+router.get('/orders/recent', signinRequired, recent_orders);
 router.get('/:year/orders-per-month', signinRequired, order_per_month);
+router.get('/:year/amount-per-month', signinRequired, amount_per_month);
 
 
 export default router;
